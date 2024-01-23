b45e9bc440650ce677f65155f81d56419aa3eea7
clement <clement.085w@gmail.com>
2024-01-23 10:09:20 +0700

    feat: add form post for records
    Signed-off-by: clement <clement.085w@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,8 +18,12 @@
     <!-- Styles -->
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script> -->
     <!-- <script src="https://cdn.jsdelivr.net/npm/flowbite-datepicker@1.2.6/dist/js/datepicker-full.min.js"></script> -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css" integrity="sha512-ELV+xyi8IhEApPS/pSj66+Jiw+sOT1Mqkzlh8ExXihe4zfqbWkxPRi8wptXIO9g73FSlhmquFlUOuMSoXz5IRw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/theme.min.css" integrity="sha512-hbs/7O+vqWZS49DulqH1n2lVtu63t3c3MTAn0oYMINS5aT8eIAbJGDXgLt6IxDHcWyzVTgf9XyzZ9iWyVQ7mCQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css"
+        integrity="sha512-ELV+xyi8IhEApPS/pSj66+Jiw+sOT1Mqkzlh8ExXihe4zfqbWkxPRi8wptXIO9g73FSlhmquFlUOuMSoXz5IRw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/theme.min.css"
+        integrity="sha512-hbs/7O+vqWZS49DulqH1n2lVtu63t3c3MTAn0oYMINS5aT8eIAbJGDXgLt6IxDHcWyzVTgf9XyzZ9iWyVQ7mCQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 
@@ -27,87 +31,87 @@
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
-        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                @auth
+                    <a href="{{ url('/dashboard') }}"
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}"
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
+                        in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}"
+                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                    @endif @endauth
+                </div>
+            @endif
+
             @auth
-            <a href="{{ url('/dashboard') }}"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-            @else
-            <a href="{{ route('login') }}"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
-                in</a>
+                <div class="grid grid-cols-3 gap-8 w-full mx-10">
+                    <div class="bg-gray-200 col-span-2">
+                        Testing 1
 
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}"
-                class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-            @endif @endauth
-        </div>
-        @endif
+                        <form class="mx-5 py-3" action="{{ route('record.store') }}" method="POST">
+                            @csrf
+                            <div class="mb-6">
+                                <label for="email"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Activity</label>
 
-        @auth
-        <div class="grid grid-cols-3 gap-8 w-full mx-10">
-            <div class="bg-gray-200 col-span-2">
-                Testing 1
+                                <label for="category_label"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a Label</label>
+                                <select id="category_label"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option name="category_id" selected disabled>Choose a ...</option>
+                                    <option value="1">Project</option>
+                                    <option value="2">Meeting</option>
+                                    <option value="3">Unproductive Hour</option>
+                                </select>
 
-                <form class="mx-5 py-3">
-
-                    <div class="mb-6">
-                        <label for="email"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Activity</label>
-
-                        <label for="category_label"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a Label</label>
-                        <select id="category_label"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option selected disabled>Choose a ...</option>
-                            <option value="project">Project</option>
-                            <option value="meeting">Meeting</option>
-                            <option value="unproductive">Unproductive Hour</option>
-                        </select>
-
-                    </div>
-                    <div class="mb-6">
-                        <label for="description"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                        <textarea style="resize: none" id="description"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            required>
+                            </div>
+                            <div class="mb-6">
+                                <label for="description"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                                <textarea style="resize: none" id="description" name="description"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required>
                             </textarea>
+                            </div>
+                            <div class="mb-6">
+
+                                <div class="relative max-w-sm">
+
+                                    <!-- <input type="text"  id="datepicker"> -->
+                                    <p>Date: <input type="tezt" id="datepicker"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            placeholder="Select date"></p>
+
+                                </div>
+
+                            </div>
+
+                            <button type="submit"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        </form>
+
                     </div>
-                    <div class="mb-6">
-
-                        <div class="relative max-w-sm">
-                            
-                            <!-- <input type="text"  id="datepicker"> -->
-                            <p>Date: <input type="tezt" id="datepicker"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Select date"></p>
-
-                        </div>
-
+                    <div class="bg-red-300 col-span-1">Testing 2</div>
+                </div>
+            @else
+                <div
+                    class="w-50 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="w-96 mb-5 m-auto">
+                        <img src="{{ asset('images/wongko.svg') }}" alt="wonki" class="h-auto w-full">
                     </div>
-
-                    <button type="submit"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                </form>
-
-            </div>
-            <div class="bg-red-300 col-span-1">Testing 2</div>
-        </div>
-        @else
-        <div
-            class="w-50 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <div class="w-96 mb-5 m-auto">
-                <img src="{{ asset('images/wongko.svg') }}" alt="wonki" class="h-auto w-full">
-            </div>
-            <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Hola!</h5>
-            <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Welcome to Employee Activity Tracker.
-                Please <a href="{{ route('register') }}"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">register</a> or <a
-                    href="{{ route('login') }}"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">login</a> to
-                continue
-            </p>
-            {{-- <div
+                    <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Hola!</h5>
+                    <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Welcome to Employee Activity Tracker.
+                        Please <a href="{{ route('register') }}"
+                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">register</a> or <a
+                            href="{{ route('login') }}"
+                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">login</a> to
+                        continue
+                    </p>
+                    {{-- <div
                 class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
                 <a href="#"
                     class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
@@ -136,72 +140,79 @@
                     </div>
                 </a>
             </div> --}}
+                </div>
+            @endauth
+
         </div>
-        @endauth
+        <!-- <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+                <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script> -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+            integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"
+            integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script>
+            // const datepickerEl = document.getElementById('datepickerId');
+            // let isOpen = false;
 
-    </div>
-    <!-- <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js" integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        // const datepickerEl = document.getElementById('datepickerId');
-        // let isOpen = false;
+            // const datepicker = new Datepicker(datepickerEl, {
+            //     maxDate: new Date(),
+            //     minDate: calculateMinDate(), // Define this function to calculate minDate
+            //     isOpen: isOpen,
+            //     autohide: true,
+            // });
 
-        // const datepicker = new Datepicker(datepickerEl, {
-        //     maxDate: new Date(),
-        //     minDate: calculateMinDate(), // Define this function to calculate minDate
-        //     isOpen: isOpen,
-        //     autohide: true,
-        // });
+            // datepickerEl.addEventListener('click', () => {
+            //     console.log('click inside');
+            //     isOpen = !isOpen;
+            //     console.log(isOpen);
 
-        // datepickerEl.addEventListener('click', () => {
-        //     console.log('click inside');
-        //     isOpen = !isOpen;
-        //     console.log(isOpen);
+            //     // Directly control visibility using isOpen
+            //     datepicker.isOpen = isOpen;
+            //     if (!isOpen) {
+            //         console.log('hide');
+            //         datepicker.hide();
+            //     }
+            // });
 
-        //     // Directly control visibility using isOpen
-        //     datepicker.isOpen = isOpen;
-        //     if (!isOpen) {
-        //         console.log('hide');
-        //         datepicker.hide();
-        //     }
-        // });
+            // document.body.addEventListener('click', (event) => {
+            //     if (!datepickerEl.contains(event.target) && !document.querySelector('.datepicker').contains(event.target)) {
+            //         console.log('click outside');
+            //         isOpen = false;
+            //         console.log(isOpen);
+            //         datepicker.isOpen = isOpen;
+            //         if (!isOpen) {
+            //             console.log('hide');
+            //             datepicker.hide();
+            //         }
+            //     }
+            // });
 
-        // document.body.addEventListener('click', (event) => {
-        //     if (!datepickerEl.contains(event.target) && !document.querySelector('.datepicker').contains(event.target)) {
-        //         console.log('click outside');
-        //         isOpen = false;
-        //         console.log(isOpen);
-        //         datepicker.isOpen = isOpen;
-        //         if (!isOpen) {
-        //             console.log('hide');
-        //             datepicker.hide();
-        //         }
-        //     }
-        // });
-
-        function calculateMinDate() {
-            const today = new Date();
-            const minDay = new Date();
-            switch (today.getDay()) {
-                case 1:
-                    minDay.setDate(today.getDate() - 3);
-                    break;
-                default:
-                    minDay.setDate(today.getDate() - 1);
-                    break;
+            function calculateMinDate() {
+                const today = new Date();
+                const minDay = new Date();
+                switch (today.getDay()) {
+                    case 1:
+                        minDay.setDate(today.getDate() - 3);
+                        break;
+                    default:
+                        minDay.setDate(today.getDate() - 1);
+                        break;
+                }
+                return minDay;
             }
-            return minDay;
-        }
 
 
-        $(function () {
-            $("#datepicker").datepicker({ maxDate: new Date(), minDate: -1 });
-        });
-    </script>
-</body>
+            $(function() {
+                $("#datepicker").datepicker({
+                    maxDate: new Date(),
+                    minDate: -1
+                });
+            });
+        </script>
+    </body>
 
 
 
-</html>
+    </html>
