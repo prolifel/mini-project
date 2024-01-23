2772c945d61d2c8ba2befc5ed774f690048f88e8
Nabila <nabilaaini18@gmail.com>
2024-01-23 10:26:55 +0700

    add start time and end time

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,12 +89,24 @@
                                     <!-- <input type="text"  id="datepicker"> -->
                                     <p>Date: <input type="tezt" id="datepicker"
                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                            placeholder="Select date"></p>
+                                            placeholder="Select date" name="date"></p>
 
                                 </div>
 
                             </div>
+                            <div class="mb-6">
+                                <div class="relative max-w-sm">
+                                    <label for="start"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start time</label>
+                                    
+                                    <input type="time" id="appt" name="start" min="09:00" max="18:00">
 
+                                    <label for="end"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End time</label>
+                                    
+                                    <input type="time" id="appt" name="end" min="09:00" max="18:00">
+                                </div>
+                            </div>
                             <button type="submit"
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                         </form>
