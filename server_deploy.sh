ea5efb0a6c0055396d9502a9a309092fa7a1a1b0
clement <clement.085w@gmail.com>
2024-01-25 10:13:21 +0700

    feat: add server deploy script
    Signed-off-by: clement <clement.085w@gmail.com>

---
+++ server_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    # Update codebase
+    git fetch origin
+    git reset --hard origin/main
+
+    # Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+    # Migrate database
+    php artisan migrate --force
+
+    # Note: If you're using queue workers, this is the place to restart them.
+    # ...
+
+    # Clear cache
+    php artisan optimize
+
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
